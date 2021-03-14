27a699abc9ca11dd9f260610d55e282b50444bb2
Florian Lahitte <lahitte.florian@gmail.com>
2021-03-14 10:17:00 -0400

    remove client

--- client/src/components/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import { reducer as formReducer } from 'redux-form';
-
-export default combineReducers({
-  auth: auth,
-  form: formReducer,
-});
