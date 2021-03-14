5af25370226f54e8260804a205bac0df3f19bbaa
Florian Lahitte <lahitte.florian@gmail.com>
2021-03-14 10:15:44 -0400

    add client and server side

---
+++ client/src/components/pages/Dashboard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '../utils/Card';
+import Loading from '../utils/Loading';
+
+const Dashborad = () => {
+  const user = useSelector((state) => state.auth.authenticated);
+
+  if (!user) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="container">
+      <h2 className="text-center title-dashboard">Dashborad</h2>
+      <div className="row">
+        <div className="col-md-12">
+          <Card user={user} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashborad;
